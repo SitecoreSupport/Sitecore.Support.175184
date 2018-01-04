9993f53bb6e337d77d484f5102831e6c37d58385
AntonKechashin <atk@sitecore.net>
2018-01-04 14:34:23 +0200

    Added request validator

---
+++ src/Sitecore.Support.175184/Web/RequestValidators/SitecoreBackendRequestValidator.cs
@@ -0,0 +1,54 @@
+﻿namespace Sitecore.Support.Web.RequestValidators
+{
+  using System;
+  using System.Linq;
+  using System.Web;
+  using System.Web.Util;
+  using Sitecore.Diagnostics;
+
+  public class SitecoreBackendRequestValidator : RequestValidator
+  {
+    [NotNull]
+    internal static readonly string[] SitecoreTrustedUrls = new string[] { "/sitecore/shell/", "/sitecore/admin/", "/-/speak/request/" };
+
+    #region Constructors
+    public SitecoreBackendRequestValidator() : this(urlStartPartsToBypass: SitecoreTrustedUrls)
+    {
+    }
+
+    protected SitecoreBackendRequestValidator(params string[] urlStartPartsToBypass)
+    {
+      this.UrlStartPartsToBypassValidation = urlStartPartsToBypass;
+    }
+    #endregion
+
+    [NotNull]
+    protected string[] UrlStartPartsToBypassValidation { get; private set; }
+
+    public virtual bool ShouldIgnoreValidation([NotNull]string rawUrl)
+    {
+      Assert.ArgumentNotNull(rawUrl, "request");
+      return this.UrlStartPartsToBypassValidation.Any(urlToByPass => rawUrl.StartsWith(urlToByPass, StringComparison.InvariantCultureIgnoreCase));
+    }
+
+    protected override bool IsValidRequestString(HttpContext context, string value, RequestValidationSource requestValidationSource, string collectionKey, out int validationFailureIndex)
+    {
+      validationFailureIndex = 0;
+      var requestUrl = this.ExtractRequestUrl(context);
+      if (this.ShouldIgnoreValidation(requestUrl))
+      {
+        return true;
+      }
+      return base.IsValidRequestString(context, value, requestValidationSource, collectionKey, out validationFailureIndex);
+    }
+
+    public virtual string ExtractRequestUrl([CanBeNull]HttpContext context)
+    {
+      if ((context == null) || (context.Request == null))
+      {
+        return string.Empty;
+      }
+      return context.Request.RawUrl ?? string.Empty;
+    }
+  }
+}
